bdef1c7d1971690d65883f83e417d86dc8b67e03
HatsumiKusano <kusano.hatsumi@gmail.com>
2023-11-16 11:52:28 +0900

    feat: フォームから送られてきたデータを扱う

---
+++ JavaScript/controlPanel/php/category.php
@@ -0,0 +1,20 @@
+<?php
+$contents = array(
+    "top"=>array("id"=> "top", "title"=> "トップページ", "data"=> []),
+    "company"=>array("id"=> "company", "title"=> "会社概要", "data"=> [
+        array( "title"=>"企業理念", "value"=> "多様化・変化する価値観やニーズに合ったスキルを身に付け、社会と共に進化を続け、顧客満足度を向上させる" ),
+    ]),
+    "new"=>array("id"=> "new", "title"=> "新卒採用", "data"=> [
+        array( "title"=>"title_01", "value"=> "01" ),
+        array( "title"=>"title_02", "value"=> "02" ),
+        array( "title"=>"title_03", "value"=> "03" ),
+        array( "title"=>"title_04", "value"=> "04" ),
+        array( "title"=>"title_05", "value"=> "05" ),
+        array( "title"=>"title_06", "value"=> "06" ),
+        array( "title"=>"title_07", "value"=> "07" ),
+    ]),
+    "career"=>array("id"=> "career", "title"=> "キャリア採用", "data"=> []),
+    "privacy"=>array("id"=> "privacy", "title"=> "プライバシーポリシー", "data"=> []),
+    "site"=>array("id"=> "site", "title"=> "サイトポリシー", "data"=> [])
+    );
+?>
---
+++ JavaScript/controlPanel/php/item.php
@@ -0,0 +1,33 @@
+<?php
+
+// フォームから送られてきたデータを取得する
+
+$itemList = $_POST;
+$listTitle = $_POST["title"];
+print_r($itemList);
+$listValue = $_POST["value"];
+
+// データの数
+$length = count($itemList);
+// 空の配列を作成
+$array = array();
+
+// それぞれの配列を作成しデータを入れる処理
+$id=-1;
+foreach ($itemList as $item) {
+    $id++;
+    print_r($id);
+    $item = array(
+        "id" => $id,
+        "title" => $listTitle[$id],
+        "value" => $listValue[$id],
+    );
+    $array[] = $item;
+};
+// print_r($array);
+// PHPの変数をJSONの形に変える
+$newData = json_encode($array, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+// 作成したディレクトリにファイルを追加し、その中にデータを入れる
+file_put_contents("./json/item.php" , $newData);
+chmod("./json/data.json", 0755);
+?>
