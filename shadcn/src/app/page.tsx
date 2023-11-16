e1b1dccc4e00caa779f16bdafaabc81754a7ba3b
HatsumiKusano <kusano.hatsumi@gmail.com>
2023-11-16 16:18:18 +0900

    feat: shdcnのインストール〜コンポーネントの使用

--- shadcn/src/app/page.tsx
+++ shadcn/src/app/page.tsx
@@ -1,7 +1,16 @@
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+
 export default function Home() {
   return (
     <>
       <h1>Hello World!</h1>
+      <Button size="lg" className="shadow-xl">
+        Click me
+      </Button>
+      <Card className="w-80 h-10 ">
+        <p className="text-center">デザインファイルを新規作成</p>
+      </Card>
     </>
   );
 }
